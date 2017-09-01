6b362964d92362ee4a3193ab5191a858e07cbdd0
Kyle Chung <kylechun9@gmail.com>
2017-09-01 14:22:45 +0800

    add sender info; clenase texts before store

--- inbox_crawler.js
+++ inbox_crawler.js
@@ -13,36 +13,43 @@
 var full_conversation = false;
 var local_web_service = 'http://127.0.0.1:5555/data';
 
+function cleanse_str(s) {
+    return s.replace(/(^[\n ]+)|([\n ]+$)/g, '');
+}
+
 GM_registerMenuCommand('Run Inbox Crawler', function() {
     alert("Start crawling inbox!");
-    var thread_href = [];
-    var last_mesg = [];
-    var time = [];
+    var data = {};
+    data.thread_href = [];
+    data.sender = [];
+    data.last_mesg = [];
+    data.time = [];
     var mesglist = $('ul.msg-conversations-container__conversations-list.list-style-none.ember-view');
     mesglist.find('a').each(function(index) {
-        var h = $(this).attr('href');
-        time[index] = $(this).find('time.msg-conversation-listitem__time-stamp').text();
-        last_mesg[index] = $(this).find('span.msg-conversation-card__message-snippet-body').text();
-        thread_href[index] = h;
+        data.thread_href[index] = $(this).attr('href');
+        data.sender[index] = cleanse_str($(this).find('h3.msg-conversation-listitem__participant-names').text());
+        data.time[index] = cleanse_str($(this).find('time.msg-conversation-listitem__time-stamp').text());
+        data.last_mesg[index] = cleanse_str($(this).find('span.msg-conversation-card__message-snippet-body').text());
     });
-    // for full conversation:
     var i;
-    var len = time.length;
+    var len = data.time.length;
+    // for full conversation:
     if (full_conversation) {
         for (i=0; i<len; i++) {
             document.location.href = 'https://www.linkedin.com' + thread_href[i];
             //wait for page load?
         }
     }
-    // post the data to a local web service
+    // post the data to a local web service for storage
     for (i=0; i<len; i++) {
         GM_xmlhttpRequest({
             method: 'POST',
             url: local_web_service,
-            data: `time=${time[i]}&last_mesg=${last_mesg[i]}`,
+            data: `time=${data.time[i]}&sender=${data.sender[i]}&last_mesg=${data.last_mesg[i]}`,
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
             }
         });
     }
 }, 'r');
+
--- tiny_server.py
+++ tiny_server.py
@@ -20,16 +20,17 @@ def nothing_is_here():
 def data():
     try:
         time = request.form['time']
-        first_mesg = request.form['first_mesg']
-        print('Received data: {}|{}'.format(time, first_mesg))
+        sender = request.form['sender']
+        last_mesg = request.form['last_mesg']
+        print('Received data: {}|{}|{}'.format(time, sender, last_mesg))
         with sqlite3.connect(_DB_NAME) as con:
             cur = con.cursor()
             cur.execute(
                 """
-                INSERT INTO inbox_data (time, first_mesg) 
-                VALUES (?,?)
+                INSERT INTO inbox_data (time, sender, last_mesg) 
+                VALUES (?,?,?)
                 """,
-                (time, first_mesg))
+                (time, sender, last_mesg))
             con.commit()
             msg = 'Record successfully added.'
     except:
@@ -44,7 +45,7 @@ def maybe_init_db():
     if not os.path.exists(_DB_NAME):
         conn = sqlite3.connect(_DB_NAME)
         print('Successfully connect to newly created database file.')
-        conn.execute('CREATE TABLE inbox_data (time TEXT, first_mesg TEXT)')
+        conn.execute('CREATE TABLE inbox_data (time TEXT, sender TEXT, last_mesg TEXT)')
         print('Successfully created destined table.')
         conn.close()
 
